e926bb10dd23206c5b05644af680650ada9b8241
simdax <simoncornaz@gmail.com>
2016-11-27 21:13:54 +0100

    simdax.ddns.net

--- configSim/start.sh
+++ configSim/start.sh
@@ -1,5 +1,8 @@
-on peut pas lancer les process comme ça en bash,
-moi je les lance séparement comme un gros nazbrouk
+# on peut pas lancer les process comme ça en bash,
+# moi je les lance séparement comme un gros nazbrouk
 
+
+## il faut être sudo avant (casse-couillerie de ASKPASS etc. inside)
 sudo icecast -c icecast.xml &
+sleep 1
 ./darkice/darkice -c darkice.cfg
